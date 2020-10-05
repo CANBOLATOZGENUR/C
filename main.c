479587b20e753b8b1cc87db8da4da96328594b17
CANBOLATOZGENUR <40635332+CANBOLATOZGENUR@users.noreply.github.com>
2020-10-05 21:53:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+for(int i=0;i<3;i++){
+    for(int j=0;j<2-i;j++){
+        printf(" ");
+    }for(int k=0;k<i+1;k++){
+    printf("*");
+
+    }
+    for(int l=0;l<i;l++){
+    printf("*");
+    }printf("\n");}
+    int i=0;
+    while(i<3){
+        printf("  ");
+        printf("*");
+        i++;
+        printf("\n");
+            }
+    }
